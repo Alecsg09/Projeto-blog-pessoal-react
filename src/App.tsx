afbc640a412634408e9b4adbc16f2f65a84fc6ea
Alecsg09 <alecsander_g@hotmail.com>
2022-05-26 14:27:29 -0300

    Adicionando o Navbar e o Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
+import Footer from './componentes/estaticos/footer/Footer';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+
 
 
 function App() {
   return (
-   <Home />
+   <>
+   <Navbar/>
+     <Home />     
+   <Footer/>
+   
+
+   </>
   );
 }
 
