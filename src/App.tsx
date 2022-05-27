42e05be0fac45e963237924226bb99c9fe562207
Alecsg09 <alecsander_g@hotmail.com>
2022-05-27 20:27:07 -0300

    Criação das rotas para o site

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,32 @@ import Home from './paginas/home/Home';
 import './App.css';
 import Footer from './componentes/estaticos/footer/Footer';
 import Navbar from './componentes/estaticos/navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './paginas/login/Login';
+
+
+
+
 
 
 
 function App() {
   return (
-   <>
-   <Navbar/>
-     <Home />     
-   <Footer/>
-   
 
-   </>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+         
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+
+
   );
 }
 
