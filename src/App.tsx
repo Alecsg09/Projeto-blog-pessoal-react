65521e6e530708180695b84d0edebf1d2bc7ec28
Alecsg09 <alecsander_g@hotmail.com>
2022-06-01 23:58:26 -0300

    Colocando a TabPostagem

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Navbar from './componentes/estaticos/navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastrousuario/CadastroUsuario';
+import ListaTema from './componentes/temas/listatema/ListaTema';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
 
 
 
@@ -24,6 +26,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/cadastro" element={<CadastroUsuario/>} />
+          <Route path="/temas" element={<ListaTema/>} />
+          <Route path="/postagens" element={<ListaPostagem/>} />
          
         </Routes>
       </div>
